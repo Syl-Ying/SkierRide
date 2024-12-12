b8c819cbe21cfcff26d485100d6dbae2f0487df3
Sylvia Y. <ying.sh@northeastern.edu>
2024-12-11 22:44:58 -0800

    Add SkierServlet for GET /skiers/*

--- QueryServlet/src/main/java/org/sylvia/SkierServlet.java
+++ QueryServlet/src/main/java/org/sylvia/SkierServlet.java
@@ -2,8 +2,10 @@ package org.sylvia;
 
 import com.google.gson.Gson;
 import io.swagger.client.model.ResponseMsg;
+import io.swagger.client.model.SkierVertical;
 
 import java.io.*;
+import java.time.Year;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -25,97 +27,103 @@ public class SkierServlet extends HttpServlet {
         queryDao.testDynamoDbConnection();
     }
 
- /*   @Override
+    /**
+     * GET /skiers/{resortID}/seasons/{seasonID}/days/{dayID}/skiers/{skierID}
+     *  get the total vertical for the skier for the specified ski day
+     * GET /skiers/{skierID}/vertical
+     */
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         resp.setContentType("application/json");
-        String pathInfo = req.getPathInfo();
+        String path = req.getPathInfo();
 
-        // validate url path and request body
-        if (!isUrlPathValid(pathInfo, resp)) {
+        if (path == null || path.isEmpty()) {
+            writeErrorResponse(resp, HttpServletResponse.SC_BAD_REQUEST, "Missing or invalid path");
             return;
         }
 
+        String[] pathParts = path.split("/");
+        if (validateUrlPath(pathParts)) {
+            // GET /skiers/{resortID}/seasons/{seasonID}/days/{dayID}/skiers/{skierID}
+            if ("seasons".equals(pathParts[2])) {
+                int totalVertical = getDailyVerticalFromDDB(pathParts);
+                // logger.info("debug: totoVertical" + String.valueOf(totalVertical));
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.getWriter().write(gson.toJson(totalVertical));
+            } else if ("vertical".equals(pathParts[2])) {
+                // GET /skiers/{skierID}/vertical
+                String resortIDParam = req.getParameter("resortID");
+                if (resortIDParam == null || resortIDParam.isEmpty()) {
+                    writeErrorResponse(resp, HttpServletResponse.SC_BAD_REQUEST, "Missing query parameter");
+                    return;
+                }
 
-        // GET /skiers/{resortID}/seasons/{seasonID}/days/{dayID}/skiers/{skierID}
-
-        // GET /skiers/{skierID}/vertical
-
-
-
-        resp.setStatus(HttpServletResponse.SC_OK); // 200
-        // TODO: process url params in `urlParts`
-        resp.getWriter().write("200 It Works!");
-
+                int resortID = Integer.parseInt(req.getParameter("resortID"));
+                int skierID = Integer.parseInt(pathParts[1]);
+                String season = req.getParameter("season"); // optional
+                SkierVertical skierVertical = getResortVerticalFromDDB(skierID, resortID, season);
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.getWriter().write(gson.toJson(skierVertical));
+            }
+        } else {
+            writeErrorResponse(resp, HttpServletResponse.SC_BAD_REQUEST, "Invalid path format");
+        }
     }
-*/
+
     @Override
     public void destroy() {
+        queryDao.shutdown();
+        logger.info("SkierServlet destroyed and QueryDao resources released");
     }
-/*
 
-    private Boolean isUrlPathValid(String urlPath, HttpServletResponse resp) throws IOException {
-        // check if the url exists
-        if (urlPath == null || urlPath.isEmpty()) {
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);  // 404
-            resp.getWriter().write(gson.toJson(new ResponseMsg().message("Missing parameters")));
-            return false;
-        }
-
-        String[] pathParts = urlPath.split("/");
-
-        // validate the request url path according to the API spec
-        // urlPath  = "/1/seasons/2019/day/1/skier/123"
-        // pathParts = [, 1, seasons, 2019, day, 1, skier, 123]
-        if (pathParts.length != 8) {
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400
-            writeResponse(resp, "Invalid path, expected format: /{resortID}/seasons/{seasonID}/days/{dayID}/skiers/{skierID}");
-            return false;
-        }
-
-        try {
-            // Validate resortID
+    /**
+     * Validate the request url path according to the API spec
+     * @param pathParts
+     *  "/1/seasons/2019/day/1/skier/123" Eg. [, 1, seasons, 2019, day, 1, skier, 123]
+     */
+    private Boolean validateUrlPath(String[] pathParts) throws IOException {
+        boolean isValidated = true;
+        //         0        1       2           3      4     5       6      7
+        // GET /skiers/{resortID}/seasons/{seasonID}/days/{dayID}/skiers/{skierID}
+        if (pathParts.length == 8 && "seasons".equals(pathParts[2]) && "day".equals(pathParts[4])
+                && "skiers".equals(pathParts[6])) {
             int resortID = Integer.parseInt(pathParts[1]);
-            if (resortID <= 0) {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                writeResponse(resp, "Invalid resortID. Must be a positive integer.");
-                return false;
-            }
-
-            // Validate seasonID
-            String seasonID = pathParts[3];
-            int seasonYear = Integer.parseInt(seasonID);
-            int currentYear = Year.now().getValue();
-            if (seasonYear < 1900 || seasonYear > currentYear) {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                writeResponse(resp, "Invalid seasonID. Must be a valid year between 1900 and " + currentYear + " .");
-                return false;
-            }
-
-            // Validate dayID range (1-366)
+            int seasonID = Integer.parseInt(pathParts[3]);
             int dayID = Integer.parseInt(pathParts[5]);
-            if (dayID < 1 || dayID > 366) {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                writeResponse(resp, "Invalid dayID, must be between 1 and 366.");
-                return false;
+            int currentYear = Year.now().getValue();
+            if (!(resortID > 0 && seasonID >= 1900 && seasonID <= currentYear && dayID >= 1 && dayID <= 366)) {
+                isValidated = false;
             }
-
-            // Validate skierID
-            int skierID = Integer.parseInt(pathParts[7]);
-            if (skierID <= 0) {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                writeResponse(resp, "Invalid skierID. Must be a positive integer.");
-                return false;
+        } else if (pathParts.length == 3 && "vertical".equals(pathParts[2])) {
+            // GET /skiers/{skierID}/vertical
+            int skierID = Integer.parseInt(pathParts[1]);
+            if (!(skierID >= 1 && skierID <= 100000)) {
+                isValidated = false;
             }
-        } catch (NumberFormatException e) {
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            writeResponse(resp, "Invalid parameter format. resortID, dayID, and skierID must be integers.");
-            return false;
+        } else {
+            isValidated =  false;
         }
 
-        return true;
+        return isValidated;
     }
-*/
+
+    private Integer getDailyVerticalFromDDB(String[] pathParts) {
+        String seasonID = pathParts[3];
+        int dayID = Integer.parseInt(pathParts[5]);
+        int skierID = Integer.parseInt(pathParts[7]);
+
+        return queryDao.getDailyVertical(seasonID, dayID, skierID);
+    }
+
+    /**
+     * GET /skiers/{skierID}/verticaL
+     * get the total vertical for the skier for specified seasons at the specified resort
+     */
+        private SkierVertical getResortVerticalFromDDB(Integer skierID, Integer resortID, String season) {
+        SkierVertical skierVertical = queryDao.getResortVertical(String.valueOf(skierID), String.valueOf(resortID), season);
+        return skierVertical;
+    }
 
     private void writeErrorResponse(HttpServletResponse resp, int statusCode, String message) throws IOException {
         resp.setStatus(statusCode);
