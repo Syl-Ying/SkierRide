8b65345e8d3e6bf23fa78a423db986b3e2436af3
Sylvia Y. <ying.sh@northeastern.edu>
2024-12-12 19:42:08 -0800

    Integreate Redis caching for GET APIs

---
+++ QueryServlet/src/main/java/org/sylvia/config/RedisConfig.java
@@ -0,0 +1,7 @@
+package org.sylvia.config;
+
+public class RedisConfig {
+    public static final String REDIS_URI = "35.94.50.248"; // not elastic ip!!
+    public static final Integer PORT = 6379;
+
+}
